f2f708960161b43a782a780e6bb9a904cb46fe6e
roy <roybercos3@gmail.com>
2019-09-24 20:43:49 +0700

    nambah fungsi profil
    masih ada error di foto profil

--- resources/views/profil/edit.blade.php
+++ resources/views/profil/edit.blade.php
@@ -3,32 +3,55 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-12">
+        <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Ubah Profil User
+                <div class="card-header">Profil {{$users->name}}</div>
+
+                <div class="card-body">
+                    <form action="{{route('profil.update',['$id'=>$users->id])}}" method="POST" enctype="multipart/form-data">
+                        <input type="hidden" name="_method" value="PUT">
+                        @csrf
+                        <div class="form-group row">
+                            <label class="col-md-4 col-form-label text-md-right">Username</label>
+                            <fieldset disabled>
+                                <div class="col-md-12">
+                                    <input id="name" type="text" class="form-control" name="name" value="{{$users->name}}">
+                                </div>
+                            </fieldset>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-md-4 col-form-label text-md-right">E-mail</label>
+                                <fieldset disabled>
+                                <div class="col-md-12">
+                                    <input id="email" type="text" class="form-control" name="email" value="{{$users->email}}">
+                                </div>
+                            </fieldset>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-md-4 col-form-label text-md-right">Nama Lengkap</label>
+                                <div class="col-md-4">
+                                    <input id="nama_lengkap" type="text" class="form-control" name="nama_lengkap" value="{{$users->nama_lengkap}}">
+                                 </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-md-4 col-form-label text-md-right">Alamat</label>
+                                <div class="col-md-4">
+                                    <input id="alamat" type="text" class="form-control" name="alamat" value="{{$users->Alamat}}">
+                                </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-md-4 col-form-label text-md-right">Foto User</label>
+                            <div class="col-md-4">
+                                <input type="file" value="{{$users->foto}}" id="foto" name="foto" placeholder="Foto">
+                            </div>
+                        </div>
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">Ubah Profil</button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
-                <form action="#" method="POST" enctype="multipart/form-data">
-                    <input type="hidden" name="_method" value="PUT">
-                    @csrf
-                    <fieldset disabled>
-                    <div class="form-group">
-                        <label>E-Mail</label>
-                        <input type="text" class="form-control" value="{{$users->email}}" id= "email" name="nama" placeholder="E-mail">
-                    </div>
-                    </fieldset>
-                    <div class="form-group">
-                        <label>Nama Lengkap</label>
-                        <input type="text" class="form-control" value="{{$users->name}}" id= "nama_lengkap" name="nama_lengkap" placeholder="Nama Lengkap">
-                    </div>
-                    <div class="form-group">
-                        <label>Alamat</label>
-                        <input type="text" class="form-control" value="{{$users->alamat}}" id= "alamat" name="alamat" placeholder="Alamat">
-                    </div>
-                    <label>Upload Foto User</label>
-                    <br>
-                    <input type="file" value="{{$users->foto}}" id="foto" name="foto" placeholder="Foto">
-                    <input type="submit" class="btn btn-primary btn-sm btn-block" value="simpan">
-                </form>
             </div>
         </div>
     </div>
--- resources/views/profil/index.blade.php
+++ resources/views/profil/index.blade.php
@@ -5,70 +5,61 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
-
+                <div class="card-header">Profil {{$users->name}}</div>
                 <div class="card-body">
-                    <form method="POST" action="#">
-                        @csrf
-
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                            <label class="col-md-4 col-form-label text-md-right">Username</label>
+                            <fieldset disabled>
+                                <div class="col-md-12">
+                                    <input id="name" type="text" class="form-control" name="name" value="{{$users->name}}">
+                                </div>
+                            </fieldset>
                         </div>
-
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                            <label class="col-md-4 col-form-label text-md-right">E-mail</label>
+                                <fieldset disabled>
+                                <div class="col-md-12">
+                                    <input id="email" type="text" class="form-control" name="email" value="{{$users->email}}">
+                                </div>
+                            </fieldset>
                         </div>
-
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                            <label class="col-md-4 col-form-label text-md-right">Nama Lengkap</label>
+                            <fieldset disabled>
+                                <div class="col-md-12">
+                                    <input id="nama_lengkap" type="text" class="form-control" name="nama_lengkap" value="{{$users->nama_lengkap}}">
+                                 </div>
+                            </fieldset>
                         </div>
-
                         <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
+                            <label class="col-md-4 col-form-label text-md-right">Alamat</label>
+                            <fieldset disabled>
+                                <div class="col-md-12">
+                                    <input id="alamat" type="text" class="form-control" name="alamat" value="{{$users->alamat}}">
+                                </div>
+                            </fieldset>
                         </div>
-
+                        <div class="form-group row">
+                            <label class="col-md-4 col-form-label text-md-right">Foto User</label>
+                            <fieldset disabled>
+                                    <div class="col-md-12">
+                                    @if ($users->foto == null)
+                                    <td>
+                                        <img src="storage/asd.jpg" width="100" height="100">
+                                    </td>
+                                @else
+                                <td>
+                                    <img src="{{asset('storage/'.$item->foto_user)}}" width="100" height="60">
+                                </td>
+                                @endif
+                                    </div>
+                            </fieldset>
+                        </div>
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
+                                    <td><a href="{{route('profil.edit',['id'=>$users->id])}}" class="btn btn-sm btn-success">Ubah Profil</a>
                             </div>
                         </div>
-                    </form>
                 </div>
             </div>
         </div>
